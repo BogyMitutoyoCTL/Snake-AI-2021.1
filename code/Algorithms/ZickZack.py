1741aed8df1f04bba4d95bc934154fb71922f4ec
pi <janzimmerer@gmx.de>
2021-03-22 14:09:31 +0100

    Test vervollständigung

--- code/Algorithms/ZickZack.py
+++ code/Algorithms/ZickZack.py
@@ -5,9 +5,17 @@ from GameData import GameData
 class ZickZack(Algorithm):
     def __init__(self):
         super().__init__()
-        self.fahre = ["north"] * 10 + ["west"] * 5
+        self.fahre = ["north"] * 10 + ["west"] * 5 + 10 * (["south"] + ["east"] * 8 + ["south"] + ["west"] * 8) + ["south"] + 9 * ["east"] + 19 * ["north"] + 9 * ["west"]
 
     def decide(self, info: GameData) -> str:
-        action = self.fahre[0]
+        if len(self.fahre) > 0:
+            action = self.fahre[0]
         del self.fahre[0]
+        if len(self.fahre) == 0:
+            self.fahre = ["south"] + 10 * (["east"] * 8 + ["south"] + ["west"] * 8 + ["south"]) + 9 * ["east"] + 19 * ["north"] + 9 * [
+                             "west"]
+
         return action
+
+
+
