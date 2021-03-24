204cf19df31fdf21f28230813bb9268ce568b534
tweller <thomas.weller@mitutoyo-ctl.de>
2021-03-24 08:03:01 +0100

    Dateiname automatisch bestimmen

--- code/decisionrecorder/main.py
+++ code/decisionrecorder/main.py
@@ -3,15 +3,19 @@ from decisionrecorder.Configuration import Configuration
 from decisionrecorder.SaveCombinations import SaveCombinations
 from decisionrecorder.TrainingDataGui import TrainingDataGui
 
-configuration_file = "configuration.json"
-output_file = "outputNina.json"
-
 if __name__ == "__main__":
+    configuration_file = "configuration.json"
     configuration = Configuration(configuration_file)
 
     builder = CombinationBuilder(configuration)
 
     combinations = builder.get_combinations()
+
+    size = f"{configuration.field_size}x{configuration.field_size}"
+    mask = f"{configuration.mask_string}"
+    directions = f"{configuration.food_directions} dir"
+    worker = f"{configuration.number_worker} of {configuration.number_parallel_workers}"
+    output_file = f"{size} {mask}, {directions}, {worker}.json"
 
     gui = TrainingDataGui(combinations=combinations,
                           configuration=configuration,
